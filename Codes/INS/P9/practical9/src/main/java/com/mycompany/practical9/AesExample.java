43669e071f3d8c6c032a8b85cac26e40600b496c
sidwak <61682424+sidwak@users.noreply.github.com>
2024-08-07 11:47:34 +0530

    Create INS_P9

---
+++ Codes/INS/P9/practical9/src/main/java/com/mycompany/practical9/AesExample.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practical9;
+
+import java.io.File; 
+import java.io.FileInputStream; 
+import java.io.FileOutputStream; 
+import java.io.IOException; 
+import java.security.InvalidKeyException; 
+import java.security.Key; 
+import java.security.NoSuchAlgorithmException; 
+import java.util.Date; 
+import javax.crypto.BadPaddingException; 
+import javax.crypto.Cipher; 
+import javax.crypto.IllegalBlockSizeException; 
+import javax.crypto.NoSuchPaddingException; 
+import javax.crypto.spec.SecretKeySpec; 
+
+/**
+ *
+ * @author Vaishanvi
+ */
+public class AesExample {
+    static void fileProcessor(int cipherMode,String key,File inputFile,File outputFile){ 
+        try { 
+            Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(cipherMode, secretKey);
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+            byte[] outputBytes = cipher.doFinal(inputBytes); 
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(outputBytes);
+            inputStream.close();
+            outputStream.close();
+        } 
+        catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+    public static void main(String[] args) 
+    { 
+        long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory(); 
+        long lStartTime = new Date().getTime(); 
+        String key = "This is a secret"; 
+        File inputFile = new File("text.txt"); 
+        File encryptedFile = new File("text.encrypted"); 
+        File decryptedFile = new File("decrypted-text.txt"); 
+        try
+        { 
+            AesExample.fileProcessor(Cipher.ENCRYPT_MODE,key,inputFile,encryptedFile); 
+            AesExample.fileProcessor(Cipher.DECRYPT_MODE,key,encryptedFile,decryptedFile);
+            System.out.println("Success"); 
+        } 
+        catch (Exception ex) { 
+            System.out.println(ex.getMessage()); 
+            ex.printStackTrace(); 
+        } 
+        long lEndTime = new Date().getTime(); 
+        long difference = lEndTime - lStartTime; 
+        System.out.println("Elapsed milliseconds: " + difference); 
+        long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long actualMemUsed = afterUsedMem - beforeUsedMem; 
+        System.out.println("Memory used: " + actualMemUsed); 
+    }
+}
---
+++ Codes/INS/P9/practical9/src/main/java/com/mycompany/practical9/Practical9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical9;
+
+/**
+ *
+ * @author Vaishanvi
+ */
+public class Practical9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
